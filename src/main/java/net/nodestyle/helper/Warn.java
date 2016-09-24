f7570734c508ac0de3ae8ccadcc454f871f748d0
mkg20001 <mkg20001@gmail.com>
2016-09-24 16:19:20 +0200

    Fix License Header

--- src/main/java/net/nodestyle/helper/Warn.java
+++ src/main/java/net/nodestyle/helper/Warn.java
@@ -15,25 +15,8 @@
  *         You should have received a copy of the GNU General Public License
  *         along with NodeStyle.  If not, see <http://www.gnu.org/licenses/>.
  */
-/**
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package net.nodestyle.helper;
 
-/**
- * Created by maciej on 24.09.16.
- */
 public class Warn {
     public static void write(Integer line,String t,String... msg2) {
         String msg=null;
--- src/main/java/net/nodestyle/path/path.java
+++ src/main/java/net/nodestyle/path/path.java
@@ -15,20 +15,6 @@
  *         You should have received a copy of the GNU General Public License
  *         along with NodeStyle.  If not, see <http://www.gnu.org/licenses/>.
  */
-/**
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package net.nodestyle.path;
 
 import java.io.File;
@@ -37,9 +23,6 @@ import java.util.regex.Pattern;
 
 import static com.sun.javafx.PlatformUtil.isWindows;
 
-/**
- * Created by maciej on 23.09.16.
- */
 class Helper {
     public static String unixSeperator="/";
     public static String replaceLast(String input, String regex, String replacement) {
